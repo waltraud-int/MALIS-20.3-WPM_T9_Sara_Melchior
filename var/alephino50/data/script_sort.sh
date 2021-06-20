dfb575c681d731c75bc13483eb357e0ee9ed5679
waltraud-int <sara.melchior@posteo.de>
2021-06-20 08:32:15 +0200

    add script

---
+++ var/alephino50/data/script_sort.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Skript zur Dateiensortierung
+# Updatedateien aus dem Ordner mit dem höchsten Datum sollen in /data verschoben werden
+# ältere noch dort befindliche Dateien sollen dabei ersetzt werden
+
+
+
+filenames='*096.*'
+
+for filename in $filenames
+do 
+  mv 200616/$filenames ../
+done
+
+echo All done
